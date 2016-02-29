c0ed796fa6a8eb6f04eee6a2de8b5bdc335ee396
Vivian Poulin <poulin@lapth.cnrs.fr>
2016-02-29 13:32:58 +0100

    add work to test commit

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/poulin/Documents/Labo/BBN/src
-# Build directory: /Users/poulin/Documents/Labo/BBN/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
